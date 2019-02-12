1f914c89cf03ae84f0e6cb819991b1b8346f4dd2
Bus1k <busiki@interia.pl>
2019-02-12 19:40:50 +0100

    show game results

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const game = {
 const hands = [...document.querySelectorAll(".select img")];
 document.querySelector(".start").addEventListener('click', startGame);
 
-//User choice function
+//User choice
 function handSelection() {
     game.playerHand = this.dataset.option;
     hands.forEach(hand => hand.style.boxShadow = '');
@@ -22,15 +22,51 @@ function handSelection() {
 
 //Computer choice
 function computerChoice() {
-    const compHand = hands[Math.floor(Math.random() * 3)];
+    const compHand = hands[Math.floor(Math.random() * 3)].dataset.option;
     return compHand;
 }
 
-//Game Start
+//Checking the result
+function checkResult(player, computer) {
+    if (player === computer) {
+        return 'draw';
+    } else if ((player === "paper" && computer === "rock") || (player === "rock" && computer === "scissors") || (player === "scissors" && computer === "paper")) {
+        return 'win';
+    } else {
+        return 'loose';
+    }
+}
+
+//Show result 
+function publishResult(player, computer, result) {
+    document.querySelector('[data-summary="your-choice"]').textContent = player;
+    document.querySelector('[data-summary="pc-choice"]').textContent = computer;
+    // document.querySelector('[data-summary="who-win"]').textContent = result;
+
+    document.querySelector('p.numbers span').textContent = ++gameSummary.numbers;
+
+    if (result === "win") {
+        document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+        document.querySelector('[data-summary="who-win"]').textContent = 'YOU ;)';
+        document.querySelector('[data-summary="who-win"]').style.color = 'green';
+    } else if (result === "loose") {
+        document.querySelector('p.losses span').textContent = ++gameSummary.losses;
+        document.querySelector('[data-summary="who-win"]').textContent = 'COMPUTER ;(';
+        document.querySelector('[data-summary="who-win"]').style.color = 'red';
+    } else {
+        document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+        document.querySelector('[data-summary="who-win"]').textContent = 'DRAW';
+        document.querySelector('[data-summary="who-win"]').style.color = 'black';
+    }
+}
+
+//Game Start - calling other functions
 function startGame() {
     if (!game.playerHand) return alert("Choose a sign!!");
 
     game.computerHand = computerChoice();
+    const gameResult = checkResult(game.playerHand, game.computerHand);
+    publishResult(game.playerHand, game.computerHand, gameResult);
 }
 
 hands.forEach(hand => hand.addEventListener('click', handSelection));
